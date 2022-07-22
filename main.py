2f6b4aa583d33689bf3d55d30282fe61d3a6279c
Larrygf02 <raulgf_02@hotmail.com>
2022-07-21 22:35:59 -0500

    create segment turtle and move

--- main.py
+++ main.py
@@ -1,19 +1,35 @@
 from turtle import Screen, Turtle
+import time
 
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("My Snake Game")
+screen.tracer(0)
 
-segment_1 = Turtle(shape="square")
-segment_1.color("white")
+starting_positions = [(0,0), (-20,0), (-40,0)]
 
-segment_2 = Turtle(shape="square")
-segment_2.color("white")
-segment_2.goto(-20, 0)
+segments = []
 
-segment_3 = Turtle(shape="square")
-segment_3.color("white")
-segment_3.goto(-40, 0)
+for position in starting_positions:
+    new_segment = Turtle("square")
+    new_segment.color("white")
+    new_segment.penup()
+    new_segment.goto(position)
+    segments.append(new_segment)
+
+
+game_is_on = True
+while game_is_on:
+    screen.update()
+    time.sleep(1)
+    
+    for seg_num in range(len(segments)-1,0,-1):
+        new_x = segments[seg_num - 1].xcor()
+        new_y = segments[seg_num - 1].ycor()
+        segments[seg_num].goto(new_x, new_y)
+    segments[0].forward(20)
+    #for seg in segments:
+    #    seg.forward(20)
 
 screen.exitonclick()
