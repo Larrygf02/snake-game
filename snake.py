8320c5d2dfefa3b83d81551f863a73d7c5b887e4
Larrygf02 <raulgf_02@hotmail.com>
2022-07-21 23:11:33 -0500

    create move snake

--- snake.py
+++ snake.py
@@ -1,11 +1,17 @@
+from tkinter import LEFT, RIGHT
 from turtle import Turtle
 STARTING_POSITIONS = [(0,0), (-20,0), (-40,0)]
 MOVE_DISTANCE = 20
+UP = 90
+DOWN = 270
+LEFT = 180
+RIGHT = 0
 
 class Snake():
 
     def __init__(self):
         self.segments = self.init_body_snake()
+        self.head = self.segments[0]
 
     def init_body_snake(self):
         segments = []
@@ -23,5 +29,21 @@ class Snake():
             new_x = self.segments[seg_num - 1].xcor()
             new_y = self.segments[seg_num - 1].ycor()
             self.segments[seg_num].goto(new_x, new_y)
-        self.segments[0].forward(20)
+        self.segments[0].forward(MOVE_DISTANCE)
+
+    def up(self):
+        if self.head.heading() != DOWN:
+            self.head.setheading(UP)
+
+    def down(self):
+        if self.head.heading() != UP:
+            self.head.setheading(DOWN)
+    
+    def left(self):
+        if self.head.heading() != RIGHT:
+            self.head.setheading(LEFT)
+    
+    def right(self):
+        if self.head.heading() != LEFT:
+            self.head.setheading(RIGHT)
                 
