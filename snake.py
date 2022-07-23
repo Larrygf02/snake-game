c9a1f5cfd7149cfd4edf3530b16f5d9195d20ae3
Larrygf02 <raulgf_02@hotmail.com>
2022-07-23 13:14:28 -0500

    Extend snake

--- snake.py
+++ snake.py
@@ -10,18 +10,15 @@ RIGHT = 0
 class Snake():
 
     def __init__(self):
-        self.segments = self.init_body_snake()
+        self.segments = []
+        self.init_body_snake()
         self.head = self.segments[0]
 
     def init_body_snake(self):
         segments = []
 
         for position in STARTING_POSITIONS:
-            new_segment = Turtle("square")
-            new_segment.color("white")
-            new_segment.penup()
-            new_segment.goto(position)
-            segments.append(new_segment)
+            self.add_segment(position)
         return segments
 
     def move(self):
@@ -30,6 +27,16 @@ class Snake():
             new_y = self.segments[seg_num - 1].ycor()
             self.segments[seg_num].goto(new_x, new_y)
         self.segments[0].forward(MOVE_DISTANCE)
+
+    def add_segment(self, position):
+        new_segment = Turtle("square")
+        new_segment.color("white")
+        new_segment.penup()
+        new_segment.goto(position)
+        self.segments.append(new_segment)
+
+    def extend(self):
+        self.add_segment(self.segments[-1].position())
 
     def up(self):
         if self.head.heading() != DOWN:
