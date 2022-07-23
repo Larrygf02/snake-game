a17a14f7b9b0dbc051b13b0f579fa7743d7203ad
Larrygf02 <raulgf_02@hotmail.com>
2022-07-23 11:24:33 -0500

    scoreboard

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from turtle import Screen, Turtle
 from snake import Snake
 from food import Food
+from scorebeard import Scorebeard
 import time
 
 screen = Screen()
@@ -11,6 +12,7 @@ screen.tracer(0)
 
 snake = Snake()
 food = Food()
+scorebeard = Scorebeard()
 
 screen.listen()
 screen.onkey(snake.up, "Up")
@@ -28,5 +30,6 @@ while game_is_on:
     ## detect collision
     if snake.head.distance(food) < 15:
         food.refresh()
+        scorebeard.update_score()
 
 screen.exitonclick()
